4d96f45b1cbfe4b63de2c92ebac0c600a2253142
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-05-23 12:01:31 +0530

    Drew Shapes
    Drew a green circle, a red square and a blue triangle using sfml library.

--- Space-Invaders/main.cpp
+++ Space-Invaders/main.cpp
@@ -19,7 +19,25 @@ int main() {
                 window->close();
         }
 
-        window->clear(sf::Color::Blue);
+        window->clear();
+
+        sf::CircleShape circle(50); // Radius 50
+        circle.setFillColor(sf::Color::Green);
+        circle.setPosition(400, 350); // Set position
+
+        sf::RectangleShape square(sf::Vector2f(70, 70)); 
+        square.setFillColor(sf::Color::Red); 
+        square.setPosition(200, 100); // Set position
+
+        sf::CircleShape triangle(80,3); 
+        triangle.setFillColor(sf::Color::Blue); 
+        triangle.setPosition(500, 100); // Set position 
+
+        window->draw(square);
+
+        window->draw(triangle);
+
+        window->draw(circle);
 
         window->display();
     }
