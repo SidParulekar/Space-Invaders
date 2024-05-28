fcd1fe5a9a9b876752abcb4d7b63f52cfe6e65ec
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-05-28 13:01:43 +0530

    Created the Service Locator
    Created the Service Locator header file and the ServiceLocator class in the header file and declared the necessary private and public methods.
    
    Created the ServiceLocator .cpp file and defined the functions declared in the header file.

---
+++ Space-Invaders/Source/ServiceLocator.cpp
@@ -0,0 +1,36 @@
+#include "C:\Users\sidpa\Documents\GitHub\Space-Invaders\Space-Invaders\Header\ServiceLocator.h";
+
+ServiceLocator::~ServiceLocator()
+{
+	//Destructor to clean up resources on object deletion
+}
+
+void ServiceLocator::createServices()
+{
+	//creates instances of Services
+}
+
+void ServiceLocator::clearAllServices()
+{
+	//deletes service instances and deallocates memory
+}
+
+ServiceLocator* ServiceLocator::getInstance()
+{
+	return nullptr;
+}
+
+void ServiceLocator::initialize()
+{
+	//initializes service locator 
+}
+
+void ServiceLocator::update()
+{
+	//updates services
+}
+
+void ServiceLocator::render()
+{
+	// renders the services
+}
