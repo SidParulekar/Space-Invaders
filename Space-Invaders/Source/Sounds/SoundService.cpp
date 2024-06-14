826c4002bea29d6bb7d6ce94342b229aed2ed13f
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-06-14 17:46:25 +0530

    Created the Sound Service
    Created Sounds folder. Created the SoundService header and source files in the Sounds folder. Created the Sound namespace and created the  SoundService class and the enum class for Sound types in the namespace in the SoundService header file. Declared and defined the necessary variables and functions to load and play background music and sound effects in the SoundService header and source files.

---
+++ Space-Invaders/Source/Sounds/SoundService.cpp
@@ -0,0 +1,24 @@
+#include "C:\Users\sidpa\Documents\GitHub\Space-Invaders\Space-Invaders\Header\Sounds\SoundService.h"
+
+namespace Sound
+{
+	void SoundService::initialize()
+	{
+	}
+
+	void SoundService::loadBackgroundMusicFromFile()
+	{
+	}
+
+	void SoundService::loadSoundFromFile()
+	{
+	}
+
+	void SoundService::playSound(SoundType)
+	{
+	}
+
+	void SoundService::playBackgroundMusic()
+	{
+	}
+}
