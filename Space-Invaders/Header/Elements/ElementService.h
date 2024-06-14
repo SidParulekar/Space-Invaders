79e7039a73780bfe1dc4b28fa9a85118664f293a
SidParulekar <76064146+SidParulekar@users.noreply.github.com>
2024-06-14 16:23:15 +0530

    Spawned multiple Bunkers
    Declared and defined a vector to store instances of BunkerData struct with specified bunker spawning positions as parameters and vector to store pointers to instances of BunkerController class and functions to update, render and delete all the instances of BunkerController class stored in the vector in the ElementService header and source files.
    
    Linked the Bunker Controller to the Element Service by including the BunkerController header file in the ElementService header file.

--- Space-Invaders/Header/Elements/ElementService.h
+++ Space-Invaders/Header/Elements/ElementService.h
@@ -1,10 +1,24 @@
 #pragma once
+#include <vector>
+#include <SFML/System/Vector2.hpp>
+#include "C:\Users\sidpa\Documents\GitHub\Space-Invaders\Space-Invaders\Header\Elements\Bunker\BunkerController.h"
+#include "C:\Users\sidpa\Documents\GitHub\Space-Invaders\Space-Invaders\Header\Elements\Bunker\BunkerModel.h"
 
 namespace Element
 {
 	class ElementService
 	{
 	private:
+		const std::vector<Bunker::BunkerData> bunker_data_list = { Bunker::BunkerData(sf::Vector2f(130.f, 800.f)), 
+																Bunker::BunkerData(sf::Vector2f(430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(730.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1130.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1430.0f, 800.f)),
+																Bunker::BunkerData(sf::Vector2f(1730.0f, 800.f)) };
+
+		std::vector<Bunker::BunkerController*> bunker_list; 
+
+		void destroy();
 
 	public:
 		ElementService();
